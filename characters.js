21ed94b60de5a058d90c3f5fa301eed66eddbc44
TosenSoft <tosensofteam@gmail.com>
2017-06-08 10:50:34 +0200

    Delete characters.js
    Reorganization

--- characters.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-
-//create a local DB to store data until the character is ready
-//to be inserted into the server DB
-export const Characters = new Mongo.Collection(null);
-export const CharactersFinal = new Mongo.Collection('finalchar');
